e347ecca57dc66d2d4a5b527d145d32aa59d9f5e
Iván de Gracia <ivan.degracia@bbva.com>
2017-12-11 10:07:29 +0100

    added diets, appointments messages routes

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const methodOverride            = require('method-override');
 const cors                      = require('cors');
 const cookieParser              = require('cookie-parser');
 const expressValidation         = require('express-validation');
-//const router              = require('./router');
 const AuthenticationController  = require('./controllers/auth');
 const UserController            = require('./controllers/user');
+const AppointmentController     = require('./controllers/appointment');
+const MessageController         = require('./controllers/message');
+const DietController            = require('./controllers/diet');
 const Promise                   = require('bluebird');
 const mongoose                  = require('mongoose');
 const { port }                  = require('./config/express');
@@ -63,7 +65,7 @@ apiRoutes.get('/', (req, res) => {
 apiRoutes.post('/auth/user', AuthenticationController.userAuth);
 
 // Reset password route
-apiRoutes.post('/auth/reset-password', AuthenticationController.forgotPassword);
+apiRoutes.post('/auth/reset_password', AuthenticationController.forgotPassword);
 
 // Refresh Token
 apiRoutes.post('/auth/refresh_token', AuthenticationController.refreshToken);
@@ -74,14 +76,69 @@ apiRoutes.post('/auth/refresh_token', AuthenticationController.refreshToken);
   //= ========================
 */
 
-// register new user route (POST http://localhost:4500/api/users)
-apiRoutes.post('/users', UserController.register);
+// Get all users route (GET http://localhost:4500/api/users?role=:role)
+apiRoutes.get('/users', passport.authenticate(), UserController.getAll);
 
-// Get user profile route (GET http://localhost:4500/api/users/:id)
+// Get user info route (GET http://localhost:4500/api/users/:id)
 apiRoutes.get('/users/:id', passport.authenticate(), UserController.get);
 
-// Get users profile route (GET http://localhost:4500/api/users)
-apiRoutes.get('/users', passport.authenticate(), UserController.getAll);
+// Create new user route (POST http://localhost:4500/api/users)
+apiRoutes.post('/users', UserController.create);
+
+// Modify user route (POST http://localhost:4500/api/users)
+apiRoutes.put('/users/:id', UserController.modify);
+
+/**
+  //= ========================
+  // Appointments Routes
+  //= ========================
+*/
+
+// Get user appointments route (GET http://localhost:4500/api/appointments/:id)
+apiRoutes.get('/appointments/:id', passport.authenticate(), AppointmentController.get);
+
+// Create user appointment route (POST http://localhost:4500/api/appointments)
+apiRoutes.post('/appointments?id=:id', passport.authenticate(), AppointmentController.create);
+
+// Modify user appointment route (PUT http://localhost:4500/api/appointments/:appointmentId/?id=:id)
+apiRoutes.put('/appointments/:appointmentId/?id=:id', passport.authenticate(), AppointmentController.modify);
+
+// Delete user appointment route (PUT http://localhost:4500/api/appointments/:appointmentId)
+apiRoutes.delete('/appointments/:appointmentId', passport.authenticate(), AppointmentController.delete);
+
+/**
+  //= ========================
+  // Messages Routes
+  //= ========================
+*/
+
+// Get user messages route (GET http://localhost:4500/api/messages/:id)
+apiRoutes.get('/messages/:id', passport.authenticate(), MessageController.get);
+
+// Create user message route (POST http://localhost:4500/api/messages?id=:id)
+apiRoutes.post('/messages?id=:id', passport.authenticate(), MessageController.create);
+
+// Delete user messages route (DELETE http://localhost:4500/api/messages/:messageId)
+apiRoutes.delete('/messages/:messageId', passport.authenticate(), MessageController.delete);
+
+/**
+  //= ========================
+  // Diets Routes
+  //= ========================
+*/
+
+// Get user diets route (GET http://localhost:4500/api/diets/:id)
+apiRoutes.get('/diets/:id', passport.authenticate(), DietController.get);
+
+// Create diet route (POST http://localhost:4500/api/diets?id=:id)
+apiRoutes.post('/diets?id=:id', passport.authenticate(), DietController.create);
+
+// Modify user diets route (PUT http://localhost:4500/api/diets/:dietId/?id=:id)
+apiRoutes.put('/diets/:dietId/?id=:id', passport.authenticate(), DietController.modify);
+
+// Delete user diets route (DELETE http://localhost:4500/api/diets/:dietId)
+apiRoutes.delete('/diets/:dietId', passport.authenticate(), DietController.delete);
+
 
 // View user profile route
 // userRoutes.get('/home', requireAuth, AuthenticationController.roleAuthorization(roles.ROLE_CLIENT), UserController.getUserProfile);
