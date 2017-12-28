c462941b5c38e1280d144131779d7fc09fe08add
Iván de Gracia <ivan.degracia@bbva.com>
2017-12-28 13:59:04 +0100

    defined mail controller

---
+++ controllers/mail.js
@@ -0,0 +1,20 @@
+const nodemailer      = require('nodemailer');
+const { smtpConfig }  = require('../config/mail');
+
+module.exports.sendEmail = function(user) {
+  const smtpTransport = nodemailer.createTransport(smtpConfig);
+
+  const mailOptions = {
+    to: 'jorgebaztan@dietafarma.es',
+    from: 'alta-online@dietafarma.es',
+    subject: 'DietaFarma Online: Alta nuevo usuario',
+    text: `Hola Jorge, tienes un nuevo cliente. Su email es: ${user.mail}\n\nUn cordial saludo`
+  };
+
+  smtpTransport.sendMail(mailOptions, function(err, info) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('Message sent: %s', info.messageId);
+  });
+};
