34b853aa309e1657c172a4c5e303179220971446
Iván de Gracia <ivan.degracia@bbva.com>
2017-12-28 15:32:12 +0100

    added client name and surname

--- controllers/mail.js
+++ controllers/mail.js
@@ -8,7 +8,7 @@ module.exports.sendEmail = function(user) {
     to: 'jorgebaztan@dietafarma.es',
     from: 'alta-online@dietafarma.es',
     subject: 'DietaFarma Online: Alta nuevo usuario',
-    text: `Hola Jorge, tienes un nuevo cliente. Su email es: ${user.email}\n\nUn cordial saludo`
+    text: `Hola Jorge, tienes un nuevo cliente. Su nombre es ${user.profile.name} ${user.profile.surname} y su email ${user.email}\n\nUn cordial saludo`
   };
 
   smtpTransport.sendMail(mailOptions, function(err, info) {
