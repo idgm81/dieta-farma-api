9ad261ac66eb39b1c01e1c8610d6b29fbbf6deb6
Iván de Gracia <ivan.degracia@bbva.com>
2018-08-08 21:34:51 +0200

    added beta server

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname,'public')));
 
 // enable cors
 const corsOption = {
-  origin: ['http://localhost:4200', 'https://dieta-farma-online.herokuapp.com'],
+  origin: ['http://localhost:4200', 'https://dieta-farma-online.herokuapp.com', 'https://beta-dieta-farma-online.herokuapp.com'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: true,
   optionsSuccessStatus: 200
