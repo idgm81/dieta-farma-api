322ae3687cf571071593e8c3a2619e0b38965db7
Iván de Gracia <ivan.degracia@bbva.com>
2018-09-19 20:45:01 +0200

    fix find nutritionist appointments

--- controllers/appointment.js
+++ controllers/appointment.js
@@ -26,8 +26,8 @@ module.exports.getCalendar = function(req, res) {
       return res.status(409).json({ error: 'Error al buscar las citas del usuario'});
     }
 
-    Appointment.findOne({ nutritionist: mongoose.Types.ObjectId(user.nutritionist) }, (err, nutritionistDates) => {
-      if (err || nutritionistDates.length === 0) {
+    Appointment.find({ nutritionist: mongoose.Types.ObjectId(user.nutritionist) }, (err, nutritionistDates) => {
+      if (err) {
         return res.status(409).json({ error: 'Error al buscar el calendario del nutricionista'});
       }
 
